ce48dac12edc3257d565e8aa5237c33951a9bc3c
robertarotaritei <robert_aro@yahoo.com>
2021-01-13 16:32:50 +0100

    Update User API

--- UserAPI/UserAPI/Infrastructure/SqlStatement.cs
+++ UserAPI/UserAPI/Infrastructure/SqlStatement.cs
@@ -14,19 +14,19 @@
 
         public string FindTokenAsync { get; }
 
-        public string VerifyOneAsync { get;  }
+        public string VerifyOneAsync { get; }
 
         public string VerifyUsernameAsync { get; }
 
         public SqlStatement()
         {
-            InsertAsync = @"INSERT INTO `user` (`user_name`, `password`) VALUES (@username, MD5(@password);";
-            UpdateAsync = @"UPDATE `user` SET `password` = MD5(@password) WHERE `user_name` = @username;";
+            InsertAsync = @"INSERT INTO `user` (`user_name`, `password`) VALUES (@username, AES_ENCRYPT(@password,'secret'));";
+            UpdateAsync = @"UPDATE `user` SET `password` = AES_ENCRYPT(@password,'secret') WHERE `user_name` = @username;";
             UpdateTokenAsync = @"UPDATE `user` SET `token` = @token WHERE `user_name` = @username;";
             DeleteAsync = @"DELETE FROM `user` WHERE `user_name` = @username AND `password` = @password;";
             FindOneAsync = @"SELECT * FROM `user` WHERE `id` = @id";
             FindTokenAsync = @"SELECT * FROM `user` WHERE `user_name` = @username";
-            VerifyOneAsync = @"SELECT * FROM `user` WHERE `user_name` = @username AND `password` = MD5(@password);";
+            VerifyOneAsync = @"SELECT * FROM `user` WHERE `user_name` = @username AND `password` = AES_ENCRYPT(@password,'secret');";
             VerifyUsernameAsync = @"SELECT * FROM `user` WHERE `user_name` = @username;";
         }
     }
